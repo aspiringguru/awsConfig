44dd96d6fd4c69461748d800fd8df8b4e83ffd90
aspiringguru <bmatthewtaylor@gmail.com>
2019-10-03 23:38:10 +0000

    backup

---
+++ testServer4000.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const port = 4000
+
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('Hello Node.js Server running on port '+port)
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
+
